5adc07d3c10a86d18cd50904a3555a37e423e386
EC2 Default User <ec2-user@ip-172-31-95-141.ec2.internal>
2022-06-19 06:24:20 +0000

    backup1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
+  # 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
