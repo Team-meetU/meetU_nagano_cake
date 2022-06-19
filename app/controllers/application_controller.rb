f43bf2566fbb3cff09c684aa09a818ff3098eba0
EC2 Default User <ec2-user@ip-172-31-95-141.ec2.internal>
2022-06-19 15:59:57 +0000

    admin_sessions_controller finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_root_path    # ログイン後に遷移するpathを設定
+    when Public
+      #root_path # ログイン後に遷移するpathを設定
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    admin_root_path # ログアウト後に遷移するpathを設定(publicブランチ統合後は別のリンクに変更)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+namespace :admin do
+    root to: 'homes#top'
+    #resource :custromers, only: [:index, :show, :edit, :update]
+  end
+
   # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
