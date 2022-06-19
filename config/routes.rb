e33be91ad24061b9e9265fa27cd6ccc8c5cd0c03
ryo-terada <ryo214099@gmail.com>
2022-06-19 06:06:02 +0000

    [devise導入直前]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
