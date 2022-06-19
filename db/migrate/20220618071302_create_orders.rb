ebe64a1a886aa899efa29aeb63174502f9a7ea98
EC2 Default User <ec2-user@ip-172-31-95-141.ec2.internal>
2022-06-19 02:38:36 +0000

    db_update1

--- db/migrate/20220618071302_create_orders.rb
+++ db/migrate/20220618071302_create_orders.rb
@@ -4,6 +4,7 @@ class CreateOrders < ActiveRecord::Migration[6.1]
 
       t.string :postal_code
       t.text :delivery_address
+      t.string :delivery_name
       t.string :method_of_payment
       t.integer :status
       t.integer :total_price
