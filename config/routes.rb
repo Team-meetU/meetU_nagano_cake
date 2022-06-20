f14b9732ec0e976104eef32036e16c16f255fd54
EC2 Default User <ec2-user@ip-172-31-5-105.ec2.internal>
2022-06-20 02:17:18 +0000

    controller　追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'addresses/index'
+  get 'addresses/edit'
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
