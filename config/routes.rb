29d8d0c33f29254888af3d0252ecb0cfdd2ba118
EC2 Default User <ec2-user@ip-172-31-25-137.ec2.internal>
2022-06-20 11:09:16 +0000

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
 
-namespace :admin do
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+
+
+  namespace :admin do
     root to: 'homes#top'
     #resource :custromers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :items, only: [:index, :show, :edit, :create, :update, :destroy]
   end
 
   # 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, controllers: {
-  sessions: "admin/sessions"
-}
+  # URL /admin/sign_in ...
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
