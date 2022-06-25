05cd466d5ed400104b0387c1bdcf61d901d10a19
EC2 Default User <ec2-user@ip-172-31-95-141.ec2.internal>
2022-06-25 17:34:48 +0000

    layout_update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,8 +9,10 @@ class Public::CartItemsController < ApplicationController
     @cart_item.save
     redirect_to cart_items_path
   end
+
   private
+
   def cart_item_params
-  params.require(:cart_item).permit(:item_id, :quantity)
-end
+    params.require(:cart_item).permit(:item_id, :quantity)
+  end
 end
