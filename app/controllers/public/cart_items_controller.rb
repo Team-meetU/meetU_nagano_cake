1ca9d62de33fd4c4b718afe0ab3268a69dc95ff2
EC2 Default User <ec2-user@ip-172-31-5-105.ec2.internal>
2022-06-26 01:23:39 +0000

    [items中途経過]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,8 +9,26 @@ class Public::CartItemsController < ApplicationController
     @cart_item.save
     redirect_to cart_items_path
   end
+    def update
+     @cart_item = CartItem.find(params[:id])
+     @cart_item.update(quantity: params[:cart_item][:quantity].to_i)
+     flash[:notice] = "You have updated user successfully."
+    redirect_to cart_items_path
+    end
+  def destroy
+     @cart_item = CartItem.find(params[:id])
+     @cart_item.destroy
+    redirect_to cart_items_path
+  end
+  def destroy_all
+     @cart_items = current_public.cart_items
+     @cart_items.destroy_all
+     redirect_to cart_items_path
+  end
+
   private
   def cart_item_params
   params.require(:cart_item).permit(:item_id, :quantity)
-end
+  end
+
 end
